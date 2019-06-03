982af97bbf29862acfde0cc6510c1d3389213a2e
Eswaraprasadp <peswar1701@gmail.com>
2019-06-03 20:50:12 +0530

    Speed increase with score

--- js/duet.js
+++ js/duet.js
@@ -1,21 +1,60 @@
-var canvas, raf, ctx, clock = true, theta = 0, bricks = [], gameOver = false, flag = false;
-var noOfBricksPassed = 0, score = 0, maxBricks = 1, scoreInteger = 0, landmarkBricks = 5, landmarkScoreForSpeed = 1, speed = 3;
+var canvas, raf, ctx, clock, theta, bricks = [], gameOver, flags;
+var noOfBricksPassed, score, maxBricks, scoreInteger, landmarkScoreForBricks, landmarkScoreForSpeed, speed;
 var blue = 'rgba(100, 100, 255, 1)', pink = 'rgba(255, 100, 100, 1)', red = 'rgba(250, 50, 50, 0.9)', green = 'rgba(50, 250, 50, 0.9)';
-var backgroundGray = 'rgba(200, 200, 200, 0.9)';
+var gray = 'rgba(200, 200, 200, 0.9)', white = 'rgba(250, 250, 250, 0.9)', black = 'rgba(10, 10, 10, 0.8)';
 var scoreText;
-var noOfBricksAlive = 0;
+var noOfBricksAlive;
 
-function init(){
+function load(){
 	canvas = document.getElementById("canvas");
 	ctx = canvas.getContext("2d");
 	scoreText = document.getElementById("score-text");
 
-	document.addEventListener("keydown", arrowControl);
-
+	ctx.save();
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = black;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.restore();
 	// console.log("Reached inside start()");
 	// draw();
-	raf = window.requestAnimationFrame(draw);
+	Start = new Button('Start', 'start');
+	Start.draw(green);
+	// raf = window.requestAnimationFrame(draw);
 	
+}
+function start(){
+
+	init();
+	raf = window.requestAnimationFrame(draw);
+}
+function restart(){
+	
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	init();	
+	raf = window.requestAnimationFrame(draw);
+}
+function init(){
+	theta = 0;
+	maxBricks = 1;
+	noOfBricksAlive = 0;
+	noOfBricksPassed = 0;
+
+	bricks = [];
+	noOfBricksAlive = 0;
+
+	gameOver = false;
+	score = 0;
+	scoreInteger = 0;
+	landmarkScoreForBricks = 4;
+	landmarkScoreForSpeed = 2;
+	speed = 3;
+
+	clock = true;
+	flags = [true, true];
+
+	document.removeEventListener("keydown", arrowControl);
+	document.addEventListener("keydown", arrowControl);
 }
 
 class Ball{
@@ -60,26 +99,37 @@ class Obstacle{
 }
 class Brick extends Obstacle{
 	constructor(){
-
-		var color = 'rgb(' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ')';
+		var rand1 = Math.random();
+		var rand2 = Math.random();
+		var rand3 = Math.random();
+		var color;
+		if((rand1*255 <= 20 && rand2*255 <= 20 && rand3*255 <= 20) || (Math.abs(rand1 - rand2)*255 <= 5 && Math.abs(rand2 - rand3)*255 <= 5 && Math.abs(rand3 - rand1)*255 <= 5)){
+			color = 'rgb(250, 50, 50)';
+		}
+		else{
+			color = 'rgb(' + Math.floor(rand1*255) + ',' + Math.floor(rand2*255) + ',' + Math.floor(rand3*255) + ')';
+		}
+		
 		super('rectangle', color);
 		this.color = color;
 		if(Math.random() < 0.50){
-			this.width = 60;
+			this.width = 48;
 			this.height = 40;
 
 		}
 		else{
 			this.width = 40;
-			this.height = 60;
+			this.height = 48;
 		}
+		
 		this.x = Math.random()*200 + 50 - this.width/2;
+		
 		this.y = 5;
 		this.vy = 3;
 		this.alive = true;
-		// draw();
 
 	}
+	
 	draw(){
 		ctx.save();
 		ctx.fillStyle = this.color;
@@ -133,26 +183,22 @@ class Button{
 		}
 		
 	}
-	draw(backGroundColor, noOfButtons = 1, number = 1){
+	draw(color, noOfButtons = 1, number = 1){
 		if(noOfButtons == 1)
 			this.y = canvas.height/2 - this.height/2;
 		else
 			this.y = canvas.height/2 - this.height/2 + (2*number - (noOfButtons + 1)) * this.height * 3 / 2;
-		ctx.fillStyle = 'rgba(250, 250, 250, 0.6)';
+		ctx.fillStyle = white;
 		ctx.fillRect(this.x, this.y, this.width, this.height);
 
-		ctx.fillStyle = backGroundColor;
+		ctx.fillStyle = color;
 
 		ctx.fillText(this.text, this.x, this.y + this.height - 10);
 		ctx.restore();
 
-		if(this.restart){
+		if(this.restart || this.start){
 			document.addEventListener('click', this.click.bind(this));
-		}
-		// else{
-			
-		// }
-
+		}		
 	}
 
 	click(e){
@@ -162,6 +208,10 @@ class Button{
 			if(gameOver && this.restart){
 				document.removeEventListener('click', this.click);
 				restart();
+			}
+			else if(this.start){
+				document.removeEventListener('click', this.click);
+				start();
 			}
 			// else if(Start.start){
 			// 	console.log("Inside click start action");
@@ -188,28 +238,6 @@ function isInside(pos, rect){
     return (pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.height && pos.y > rect.y);
 }
 
-function restart(){
-	
-	
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-	// bricks.length = 0;
-	theta = 0;
-	landMark = 5;
-	maxBricks = 1;
-	noOfBricksAlive = 0;
-
-	gameOver = false;
-	score = 0;
-	scoreInteger = 0;
-	landmarkBricks = 5;
-	landmarkScoreForSpeed = 1;
-	speed = 3;
-
-	document.addEventListener("keydown", arrowControl);
-
-	raf = window.requestAnimationFrame(draw);
-}
 // document.addEventListener('click', function(evt) {
 // 	var mousePos = getMousePos(canvas, evt);
 // 	if(gameOver)
@@ -237,49 +265,6 @@ function restart(){
 // 		ctx.fillText(this.text, this.x, this.y + this.height - 10);
 // 	}
 // }
-// var ballBlue = {
-// 	x: 0,
-// 	y: 0, 
-// 	radius: 10,
-// 	v: 20,
-				
-// 	color: 'rgba(100, 100, 255, 1)',
-// 	draw: function(){
-// 		ctx.beginPath();
-		// ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
-		// ctx.closePath();
-		// ctx.fillStyle = this.color;
-		// ctx.fill();
-// 					// console.log("Blue ball is drawn");
-// 	}
-// }
-// var ballPink = {
-// 	x: 0,
-// 	y: 0, 
-// 	radius: 10,
-// 	v: 20,
-// 	color: 'rgba(255, 100, 100, 1)',
-// 	draw: function(){
-// 		ctx.beginPath();
-// 		ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
-// 		ctx.closePath();
-// 		ctx.fillStyle = this.color;
-// 		ctx.fill();
-// 		// console.log("Pink ball is drawn");
-// 	}
-// }
-// var brick = {
-// 	x: 200,
-// 	y: 5,
-// 	width: 80,
-// 	height: 40,
-// 	vy: 3,
-// 	color: 'rgb(0, 0, 0)',
-// 	draw: function(){
-// 		ctx.fillStyle = this.color;
-// 		ctx.fillRect(this.x, this.y, this.width, this.height);
-// 	}
-// }
 
 function arrowControl(e) {
 	var code = e.keyCode ? e.keyCode : e.which;
@@ -302,12 +287,16 @@ function draw(){
 	rishav = new Ball(blue, true);
 	phoebe = new Ball(pink, false);
 
-	rishav.draw(150, 400, 50);
-	phoebe.draw(150, 400, 50);
+	rishav.draw(150, 400, 58);
+	phoebe.draw(150, 400, 58);
 
 	score += 1/100;
 
-	console.log('Score : ' + Math.floor(score));
+	// console.log('Score : ' + Math.floor(score));
+	if(flags[0]){
+		console.log('Bricks Passed : ' + noOfBricksPassed);
+		flags[0] = false;
+	}
 	if(score >= (scoreInteger + 1)){
 		// console.log('Inside setting, Score : ' + score);
 		scoreText.innerHTML = "Score : " + Math.floor(score);
@@ -315,20 +304,21 @@ function draw(){
 	}
 	var deletedBrick;
 
-	if(score >= landmarkBricks){
-		landmarkBricks += 5;
+	if(score >= landmarkScoreForBricks){
+		landmarkScoreForBricks += 5;
 		++maxBricks;
 	}
 	if(score >= landmarkScoreForSpeed){
 		console.log("landmarkScoreForSpeed : " + landmarkScoreForSpeed + " Reached");
+		// flags[1] = false;
 		speed += 0.1;
-		bricks.forEach(function(brick, index){
-			console.log("Entered for brick : " + (index + 1));
-			brick.updateToSpeed(speed);
-			console.log("Speed updated to : " + brick.vy.toFixed(2) + " for brick : " + (index + 1));
-		});
 		landmarkScoreForSpeed += 1;
 	}
+	// bricks.forEach(function(brick, index){
+	// 		// console.log("Entered for brick : " + (index + 1));
+	// 		brick.updateToSpeed(speed);
+			
+	// 	});
 	// if(flag){
 	// 	console.log("noOfBricksAlive < maxBricks : " + noOfBricksAlive < maxBricks);
 	// 	console.log("noOfBricksAlive >= 0 : "+ noOfBricksAlive >= 0);
@@ -346,11 +336,16 @@ function draw(){
 			// console.log("bricks[noOfBricksAlive-1].y before Generate: " + bricks[noOfBricksAlive-1].y);
 			
 			bricks.push(new Brick());
+
 			++noOfBricksAlive;
+			bricks[noOfBricksAlive - 1].updateToSpeed(speed);
+			console.log("Speed updated to : " + bricks[noOfBricksAlive - 1].vy.toFixed(2) + " for brick : " + noOfBricksAlive);
 		}
 		else if(noOfBricksAlive == 0){
 			bricks.push(new Brick());
 			++noOfBricksAlive;
+			bricks[noOfBricksAlive - 1].updateToSpeed(speed);
+			console.log("Speed updated to : " + bricks[noOfBricksAlive - 1].vy.toFixed(2) + " for brick : " + noOfBricksAlive);
 			// console.log("Since noOfBricksAlive is 0, noOfBricksAlive was incremented to " + noOfBricksAlive);
 		}			
 		
@@ -359,8 +354,9 @@ function draw(){
 	if(!bricks[0].isAlive()){
 		deletedBrick = bricks.splice(0, 1);
 		--noOfBricksAlive;
+		++noOfBricksPassed;
+		flags[0] = true;
 		console.log("One brick deleted");
-		flag = true;
 	}
 	
 	if(!gameOver){
@@ -382,45 +378,15 @@ function draw(){
 			ending();
 		}
 	});
-	// if(!gameOver){
-	// 		// gameOver = false;
-			
-	// 		bricks.forEach(function(brick, index){
-	// 			console.log("Brick number : " + index);
-			
-	// 			// bricks[i].draw();
-	// 			if(brick.isAlive()){
-	// 				brick.update();
-	// 				brick.draw();
-	// 			}
-	// 			else{
-	// 				var dummyBrick = bricks.pop(brick);
-	// 				console.log("Bricks length : " + bricks.length / Object.keys(bricks).length);
-	// 				--noOfBricksAlive;
-	// 			}
-			
-	// 			if(circleCollidesRectangle(rishav, brick) || circleCollidesRectangle(phoebe, brick)){
-	// 				gameOver = true;
-	// 				ending();
-	// 			}
-	// 			else{
-	
-	// 				raf = window.requestAnimationFrame(draw);
-	// 			}
-	// 		})
-			
-	// }
 	
 }
 function ending(){
-	bricks = [];
-	noOfBricksAlive = 0;
-	document.removeEventListener("keydown", arrowControl);
-	console.log("Inside ending and animation canceled");
+	
 	ctx.save();
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.fillStyle = backgroundGray;
+	ctx.fillStyle = black;
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.restore();
 
 	GameOver = new Button('Game Over!');
 	PlayAgain = new Button('Play Again', 'restart');
@@ -428,69 +394,14 @@ function ending(){
 	GameOver.draw(red, 2, 1);
 	PlayAgain.draw(green, 2, 2);
 }
-
-	
-	// if(!attacking){
-	// 	rand = Math.random();
-	// 		if(rand < 0.50){
-	// 			brick.width = 60;
-	// 			brick.height = 40;
-	// 		}
-	// 		else{
-	// 			brick.width = 40;
-	// 			brick.height = 60;
-	// 		}
-	// 	brick.x = Math.random()*200 + 50 - brick.width/2;
-	// 	brick.color = 'rgb(' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ')';
-	// 	// console.log("New brick created with color " + brick.color);
-	// 	attacking = true;
-								
 			
-	// }
-	// else if(brick.y + brick.vy + brick.height/2 >= 500){
-	// 	brick.y = 5;
-	// 	attacking = false;
-					
-	// }
-	// if(ballBlueCollidesRectangle() || ballPinkCollidesRectangle()){
-	// 	gameOver = true;
-	// 	console.log("Game Over!");
-	// }
-	// if(!gameOver){
-		// brick.draw();
-		// brick.y += brick.vy;
-			
-		// console.log("Ball Blue collides brick? : " + ballBlueCollidesRectangle());
-		// console.log("Ball Pink collides brick? : " + ballPinkCollidesRectangle());
-		// raf = window.requestAnimationFrame(draw);
-	// }
-	// else{
-	// 	window.cancelAnimationFrame(raf);
-	// 	// gameOver = false;
-	// 	ctx.fillStyle = 'rgba(75, 75, 75, 1)';
-	// 	// ctx.clearRect(0, 0, canvas.width, canvas.height);
-	// 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	// 	var text = 'Game Over!';
-	// 	ctx.font = '400 40px Kremlin Pro Web';
-	// 	var textWidth = ctx.measureText(text).width;
-	// 	var x = canvas.width/2 - textWidth/2;
-	// 	ctx.fillStyle = 'rgba(250, 250, 250, 0.6)';
-	// 	ctx.fillRect(x, 150, textWidth, 50);
-	// 	ctx.fillStyle = 'rgba(250, 50, 50, 1)';			    	
-	// 	ctx.fillText(text, x, 190);
-
-	// 	playAgain.draw();
-				
-	// 	ctx.restore();
-	// }
-				
 
 function drawBackground(){
 	
-	var backgroundBalls = 'rgba(200, 200, 200, 0.4)';		
+	var backgroundBalls = 'rgba(10, 10, 10, 0.4)';		
 				
 	ctx.save();
-	ctx.fillStyle = backgroundGray;
+	ctx.fillStyle = black;
 	ctx.fillRect(0, 0, canvas.width, 340);
 
 	ctx.fillRect(0, 460, canvas.width, 40);
@@ -504,36 +415,7 @@ function drawBackground(){
 
 	ctx.restore();
 }
-// function ballBlueCollidesRectangle(){
-// 	var cx = 150 + 50 * Math.cos(theta);
-// 	var cy = 400 - 50 * Math.sin(theta);
-// 	var cr = 10;
-// 	var rx = brick.x;
-// 	var ry = brick.y;
-// 	var rw = brick.width;
-// 	var rh = brick.height;
 
-// 	var distX = Math.abs(cx - rx - rw / 2);
-// 	var distY = Math.abs(cy - ry - rh / 2);
-
-// 	if (distX > (rw / 2 + cr)) {
-// 	   return false;
-// 	}
-// 	if (distY > (rh / 2 + cr)) {
-// 	   return false;
-// 	}
-
-// 	if (distX <= (rw / 2)) {
-// 	   return true;
-// 	}
-// 	if (distY <= (rh / 2)) {
-// 	   return true;
-// 	}
-
-// 	var dx = distX - rw / 2;
-// 	var dy = distY - rh / 2;
-// 	return (dx * dx + dy * dy <= (cr * cr));
-// }
 function circleCollidesRectangle(Circle, Rectangle){
 	var cx = Circle.x;
 	var cy = Circle.y;
@@ -564,34 +446,5 @@ function circleCollidesRectangle(Circle, Rectangle){
 	var dy = distY - rh / 2;
 	return (dx * dx + dy * dy <= (cr * cr));
 }
-// function ballPinkCollidesRectangle(){
-// 	var cx = 150 - 50 * Math.cos(theta);
-// 	var cy = 400 + 50 * Math.sin(theta);
-// 	var cr = 10;
-// 	var rx = brick.x;
-// 	var ry = brick.y;
-// 	var rw = brick.width;
-// 	var rh = brick.height;
 
-// 	var distX = Math.abs(cx - rx - rw / 2);
-// 	var distY = Math.abs(cy - ry - rh / 2);
-
-// 	if (distX > (rw / 2 + cr)) {
-// 	   return false;
-// 	}
-// 	if (distY > (rh / 2 + cr)) {
-// 	   return false;
-// 	}
-
-//    if (distX <= (rw / 2)) {
-// 	   return true;
-// 	}
-// 	if (distY <= (rh / 2)) {
-// 	   return true;
-// 	}
-
-// 	var dx = distX - rw / 2;
-// 	var dy = distY - rh / 2;
-// 	return (dx * dx + dy * dy <= (cr * cr));				
-// }
 
